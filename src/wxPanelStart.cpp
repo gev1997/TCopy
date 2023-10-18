11a38cdfc47d43d06816062577970d7402e67109
Gevorg Kamalyan <gevorg.kamalyan@siemens.com>
2023-10-19 01:32:30 +0400

    Construct the wxPanelStart GUI objects

--- src/wxPanelStart.cpp
+++ src/wxPanelStart.cpp
@@ -2,7 +2,12 @@
 
 wxPanelStart::wxPanelStart(wxFrame* parent)
     : wxPanelBase_{parent}
+    , mEditSource{new wxTextCtrl(this, wxID_ANY, "", {10, 10}, {405, 30})}
+    , mEditDestination{new wxTextCtrl(this, wxID_ANY, "", {10, 50}, {405, 30})}
+    , mButtonSource{new wxButton(this, wxID_ANY, "Source", {425, 10}, {70, 30})}
+    , mButtonDestination{new wxButton(this, wxID_ANY, "Destination", {425, 50}, {70, 30})}
+    , mButtonLoad{new wxButton(this, wxID_ANY, "Load", {365, 160}, {130, 40})}
+    , mSubFolders{new wxCheckBox(this, wxID_ANY, "Subfolders", {10, 90}, {90, 20})}
 {
-    mSize.Set(500, 250);
-    SetBackgroundColour(*wxRED);
+    mSize.Set(520, 250);
 }
--- src/wxPanelStart.h
+++ src/wxPanelStart.h
@@ -1,6 +1,10 @@
 #ifndef WXPANELSTART_H
 #define WXPANELSTART_H
 
+#include <wx/textctrl.h>
+#include <wx/button.h>
+#include <wx/checkbox.h>
+
 #include "wxPanelBase.h"
 
 class wxPanelStart : public wxPanelBase_
@@ -8,6 +12,14 @@ class wxPanelStart : public wxPanelBase_
 public:
     wxPanelStart(wxFrame* parent);
     virtual ~wxPanelStart() = default;
+
+private:
+    wxTextCtrl* mEditSource;
+    wxTextCtrl* mEditDestination;
+    wxButton* mButtonSource;
+    wxButton* mButtonDestination;
+    wxButton* mButtonLoad;
+    wxCheckBox* mSubFolders;
 };
 
 #endif // WXPANELSTART_H
