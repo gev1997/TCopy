825592aba0b546bcd839062150c764d2f2fa13fc
Gevorg <qamalyan86@gmail.com>
2023-11-12 15:06:08 +0400

    Minor change

--- src/wxDirPicker.h
+++ src/wxDirPicker.h
@@ -9,6 +9,7 @@ class wxDirPicker : public wxDirPickerCtrl
 {
 public:
     wxDirPicker(wxWindow* parent, const wxString& label, const wxPoint& pos, const wxSize& size);
+    virtual ~wxDirPicker() = default;
 
 private:
     void OnDirPickerChanged(wxFileDirPickerEvent& event);
