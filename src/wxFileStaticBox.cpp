79016a5e796d3458b27f1b13350509265c868179
Gevorg <qamalyan86@gmail.com>
2023-12-17 18:39:59 +0400

    Add wxCHK_3STATE for 'Select All' combo box

--- src/wxFileStaticBox.cpp
+++ src/wxFileStaticBox.cpp
@@ -3,5 +3,5 @@
 wxFileStaticBox::wxFileStaticBox(wxWindow* parent)
     : wxStaticBox{parent, wxID_ANY, wxEmptyString, {10, 30}, {330, 420}}
     , mFileListBox{new wxCheckListBox(this, wxID_ANY, {10, 30}, {310, 380})}
-    , mSelectAll{new wxCheckBox(this, wxID_ANY, "Select All", {13, 10}, {90, 20})}
+    , mSelectAll{new wxCheckBox(this, wxID_ANY, "Select All", {13, 10}, {90, 20}, wxCHK_3STATE)}
 {}
