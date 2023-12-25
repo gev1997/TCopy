0071435c3de5f1723d83e36b321e7768a8587c0c
Gevorg <qamalyan86@gmail.com>
2023-12-26 03:58:57 +0400

    Minor change: Change wxFileStaticBox size

--- src/wxFileStaticBox.cpp
+++ src/wxFileStaticBox.cpp
@@ -1,8 +1,8 @@
 #include "wxFileStaticBox.h"
 
 wxFileStaticBox::wxFileStaticBox(wxWindow* parent)
-    : wxStaticBox{parent, wxID_ANY, wxEmptyString, {10, 30}, {330, 420}}
-    , mFileListBox{new wxCheckListBox(this, wxID_ANY, {10, 30}, {310, 380})}
+    : wxStaticBox{parent, wxID_ANY, wxEmptyString, {10, 30}, {400, 420}}
+    , mFileListBox{new wxCheckListBox(this, wxID_ANY, {10, 30}, {380, 380})}
     , mSelectAll{new wxCheckBox(this, wxID_ANY, "Select All", {13, 10}, {90, 20}, wxCHK_3STATE)}
 {}
 
